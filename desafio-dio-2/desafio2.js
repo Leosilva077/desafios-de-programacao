6f853c63267bc88d147dc82c09b0677e1a4958de
leandro silva <leandrolimaamarante@gmail.com>
2024-08-08 10:57:05 -0300

    adicionando pastas e arquivos js

---
+++ desafio-dio-2/desafio2.js
@@ -0,0 +1,27 @@
+function calcularSaldoRank(vitorias, derrotas) {
+    // Calcula o saldo de Rankeadas
+    const saldoVitorias = vitorias - derrotas;
+  
+    // Determinar o nível do Jogador 
+    let nivel;
+    if (saldoVitorias < 10) {
+      nivel = "Ferro";
+    } else if (saldoVitorias <= 20) {
+      nivel = "Bronze";
+    } else if (saldoVitorias <= 50) {
+      nivel = "Prata";
+    } else if (saldoVitorias <= 80) {
+      nivel = "Ouro";
+    } else if (saldoVitorias <= 90) {
+      nivel = "Diamante";
+    } else if (saldoVitorias <= 100) {
+      nivel = "Lendário";
+    } else {
+      nivel = "Imortal";
+    }
+    
+    console.log(`O herói tem um saldo de ${saldoVitorias} e está no nível ${nivel}`);
+  }
+  
+  calcularSaldoRank(10,0 );
+  
